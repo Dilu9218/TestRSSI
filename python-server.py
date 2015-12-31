00c93d222b9cdc209ab79f73ce74588b5018c8c7
asanka-code <asanka.code@gmail.com>
2015-12-31 16:58:06 +0530

    Our python server now reads data from UDP and write to a named pipe. An octave script in a separate place will be reading data from this named file.

--- python-server.py
+++ python-server.py
@@ -1,4 +1,18 @@
+import os
+import tempfile
+import time
 import socket
+
+filename = '/home/asanka/Downloads/myfifo'
+print filename
+try:
+	os.mkfifo(filename)
+except OSError, e:
+	print "Failed to create FIFO: %s" % e
+
+bufsize=1 # line buffered where each line is flushed immediately to the file
+fifo = open(filename, 'w', bufsize)
+
 
 #UDP_IP = "192.168.1.33"
 UDP_IP = "192.168.43.97"
@@ -12,5 +26,7 @@ sock.bind((UDP_IP, UDP_PORT))
 print "Waiting..."
 
 while True:
-  data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-  print "-----------------------------------------------------\n", data
+	data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+	print "-----------------------------------------------------\n", data
+	fifo.write(data)
+	fifo.write('\n')	
